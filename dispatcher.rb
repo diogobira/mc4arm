05f718f0c1014350809d62e90f53981c62721514
diogobira <diogobira@gmail.com>
2012-12-09 11:33:37 -0200

    subindo o dispatcher

---
+++ dispatcher.rb
@@ -0,0 +1,15 @@
+
+arquivo_configuracoes = ARGV[0]
+quantidade_execucoes = ARGV[1]
+
+cashFlows = Array.new
+
+parametros = Parametros.new(arquivo_configuracoes)
+
+s = Simulador.new(parametros)
+
+(1..quantidade_execucoes).each do |i|
+	Thread.new { cashFlows << s.executar }
+end
+
+
